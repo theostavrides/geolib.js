9559b7f3c34fdb8c1cd46093f4bf683e760a0682
theostavrides <theostavrides@gmail.com>
2019-06-15 21:24:31 -0700

    fix space for style

--- geometrylib.test.js
+++ geometrylib.test.js
@@ -55,11 +55,11 @@ describe('intersects function', () => {
 // ------------------ convexHull ------------------
 
 describe('convexHull function', () => {
-  let points = [{'x':573,'y':345},{'x':306,'y':441},{'x':556,'y':332},{'x':87,'y':113},
-                {'x':131,'y':122},{'x':230,'y':154},{'x':762,'y':233},{'x':728,'y':154}];
+  let points = [{'x':573,'y':345}, {'x':306,'y':441}, {'x':556,'y':332}, {'x':87,'y':113},
+                {'x':131,'y':122}, {'x':230,'y':154}, {'x':762,'y':233}, {'x':728,'y':154}];
 
-  let expectedOutput = [{'x':87,'y':113},{'x':306,'y':441},{'x':573,'y':345},
-                        {'x':762,'y':233},{'x':728,'y':154}];
+  let expectedOutput = [{'x':87,'y':113}, {'x':306,'y':441}, {'x':573,'y':345},
+                        {'x':762,'y':233}, {'x':728,'y':154}];
 
   it('generate convex hull from random points', () => {
     expect(convexHull(points)).toStrictEqual(expectedOutput);
